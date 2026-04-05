0b432640c8e66aace7f32893cd46716a9ef15577
jiyeon kim <152959732+jiyeon-kk@users.noreply.github.com>
2026-04-05 13:23:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from api.classify import router as classify_router
+from api.summary import router as summary_router
 
 app = FastAPI()
 
 app.include_router(classify_router)
+app.include_router(summary_router)
 
 @app.get("/")
 def root():
