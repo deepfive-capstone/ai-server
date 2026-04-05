838d55fb1e768ec3292e28ee4e5d40402d8b511e
jiyeon kim <152959732+jiyeon-kk@users.noreply.github.com>
2026-04-05 13:22:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from api.classify import router as classify_router
 
+app = FastAPI()
+
+app.include_router(classify_router)
+
+@app.get("/")
+def root():
+    return {"message": "AI server is running"}
